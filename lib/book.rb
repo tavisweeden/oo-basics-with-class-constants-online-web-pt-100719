cfd7709c8cd8af2242657cb0d20c1b188a2c7047
Tavis Weeden <tavisweeden@gmail.com>
2019-11-10 21:45:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,32 +1,3 @@
-# class Book
-#   attr_accessor :author, :page_count,
-#   #attr_reader :title, :genre
-    
-  
-#   def initialize(title)
-#     @title = title
-#   end
-  
-#   def title
-#     @title
-#   end
-  
-#   def genre=(genre)
-#     @genre = genre
-#     GENRES << genre
-    
-#     def genre
-#       @genre 
-#     end
-    
-#   end
-
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end
-
-# end
-
 class Book
   attr_accessor :author, :page_count  # remove the attr_accessor for genre
   attr_reader :title, :genre  # add an attr_reader for genre
